9218459ce8966d95dc247e447d274794e6347b63
Dominic Fischer <dominicfischer7@gmail.com>
2019-02-03 21:08:53 +0000

    Fix macos loader.

--- kgl-opengl/src/macosMain/kotlin/com/kgl/opengl/utils/Loader.kt
+++ kgl-opengl/src/macosMain/kotlin/com/kgl/opengl/utils/Loader.kt
@@ -22,7 +22,5 @@ actual object Loader {
 	private val module: COpaquePointer = dlopen("/System/Library/Frameworks/OpenGL.framework/Versions/Current/OpenGL", RTLD_LAZY) ?:
 	throw Exception("'OpenGL' not found!")
 
-	fun kglGetProcAddress(name: String): COpaquePointer? = memScoped {
-		dlsym(module, name.cstr.ptr)?.reinterpret()
-	}
+	fun kglGetProcAddress(name: String): COpaquePointer? = dlsym(module, name)?.reinterpret()
 }
