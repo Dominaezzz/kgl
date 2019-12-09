37d0e632081800df46d742760e863142ccdca4b6
Dominic Fischer <dominicfischer7@gmail.com>
2019-12-09 19:25:53 +0000

    Add hack source file to trick compiler

---
+++ kgl-glfw-static/src/commonMain/kotlin/com/kgl/glfw/Hack.kt
@@ -0,0 +1,6 @@
+package com.kgl.glfw
+
+// This file is here to trick gradle or the kotlin compiler into creating a
+// KLIB, as without any source files it (reasonably) skips the compilation
+// process.
+// This will be here until one can easily create KLIBs with just a static lib.
