b28bdc3462365098557ecbca793f86febd86d74d
Dominic Fischer <dominicfischer7@gmail.com>
2019-12-22 09:39:28 +0000

    Refactor glfw-static build.gradle.kts

--- kgl-glfw-static/build.gradle.kts
+++ kgl-glfw-static/build.gradle.kts
@@ -10,21 +10,24 @@ val unzipWin64Binaries by project(":kgl-glfw").tasks.getting(Copy::class)
 val unzipMacOSBinaries by project(":kgl-glfw").tasks.getting(Copy::class)
 
 kotlin {
-	val configs = listOf(
-			mingwX64() to unzipWin64Binaries.destinationDir.resolve("lib-mingw-w64/libglfw3.a"),
-			linuxX64() to file("/usr/local/lib/libglfw3.a"),
-			macosX64() to unzipMacOSBinaries.destinationDir.resolve("lib-macos/libglfw3.a")
+	val staticLibs = mapOf(
+			KonanTarget.MINGW_X64 to unzipWin64Binaries.destinationDir.resolve("lib-mingw-w64/libglfw3.a"),
+			KonanTarget.LINUX_X64 to file("/usr/local/lib/libglfw3.a"),
+			KonanTarget.MACOS_X64 to unzipMacOSBinaries.destinationDir.resolve("lib-macos/libglfw3.a")
 	)
 
-	for ((target, staticLib) in configs) {
-		target.compilations["main"].apply {
-			if (target.konanTarget == KonanTarget.MINGW_X64){
-				compileKotlinTask.dependsOn(unzipWin64Binaries)
-			} else if (target.konanTarget == KonanTarget.MACOS_X64){
-				compileKotlinTask.dependsOn(unzipMacOSBinaries)
-			}
-			kotlinOptions {
-				freeCompilerArgs = listOf("-include-binary", staticLib.absolutePath)
+	configure(listOf(mingwX64(), linuxX64(), macosX64())) {
+		compilations {
+			"main" {
+				if (konanTarget == KonanTarget.MINGW_X64){
+					compileKotlinTask.dependsOn(unzipWin64Binaries)
+				} else if (konanTarget == KonanTarget.MACOS_X64){
+					compileKotlinTask.dependsOn(unzipMacOSBinaries)
+				}
+				val staticLib = staticLibs.getValue(konanTarget)
+				kotlinOptions {
+					freeCompilerArgs = listOf("-include-binary", staticLib.absolutePath)
+				}
 			}
 		}
 	}
