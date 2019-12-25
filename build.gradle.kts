b952a738b66756624474c4a24295c5b5c896a3df
Dominic Fischer <dominicfischer7@gmail.com>
2019-12-25 21:21:29 +0000

    Upgrade stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.konan.target.HostManager
 import java.io.ByteArrayOutputStream
 
 plugins {
-	kotlin("multiplatform") version ("1.3.60") apply false
+	kotlin("multiplatform") version ("1.3.61") apply false
 	id("de.undercouch.download") version ("3.4.3") apply false
 }
 
