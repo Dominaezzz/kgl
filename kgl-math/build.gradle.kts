1d6115db8bdac0606168146bdd9c18f977f681e9
Nicola Buescher <nicolalucasbuescher@gmail.com>
2020-02-07 10:56:23 -0500

    add implementation for FloatVector2

---
+++ kgl-math/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("multiplatform")
+    `maven-publish`
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotation"))
+            }
+        }
+    }
+}
