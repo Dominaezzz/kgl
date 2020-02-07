2de935cd75b868c8b411d9ab77ea5e9417092125
Nicola Buescher <nicolalucasbuescher@gmail.com>
2020-02-07 10:57:50 -0500

    reindent with tabs

--- kgl-math/src/commonMain/kotlin/com/kgl/math/AbstractMutableVector2.kt
+++ kgl-math/src/commonMain/kotlin/com/kgl/math/AbstractMutableVector2.kt
@@ -2,21 +2,21 @@ package com.kgl.math
 
 abstract class AbstractMutableVector2<T> : MutableVector2<T> {
 
-    override fun equals(other: Any?): Boolean {
-        if(this === other) return true
-        if (other !is AbstractMutableVector2<*>) return false
+	override fun equals(other: Any?): Boolean {
+		if(this === other) return true
+		if (other !is AbstractMutableVector2<*>) return false
 
-        if (x != other.x) return false
-        if (y != other.y) return false
+		if (x != other.x) return false
+		if (y != other.y) return false
 
-        return true
-    }
+		return true
+	}
 
-    override fun hashCode(): Int {
-        var result = x.hashCode()
-        result += 31 * y.hashCode()
-        return result
-    }
+	override fun hashCode(): Int {
+		var result = x.hashCode()
+		result += 31 * y.hashCode()
+		return result
+	}
 
-    override fun toString(): String = "[$x, $y]"
+	override fun toString(): String = "[$x, $y]"
 }
--- kgl-math/src/commonMain/kotlin/com/kgl/math/Vector2.kt
+++ kgl-math/src/commonMain/kotlin/com/kgl/math/Vector2.kt
@@ -7,39 +7,39 @@ package com.kgl.math
  */
 interface Vector2<T> {
 
-    val x: T
-    val y: T
+	val x: T
+	val y: T
 
-    operator fun get(index: Int): T = when (index) {
-        0 -> x; 1 -> y; else -> throw IndexOutOfBoundsException()
-    }
+	operator fun get(index: Int): T = when (index) {
+		0 -> x; 1 -> y; else -> throw IndexOutOfBoundsException()
+	}
 
-    operator fun component1(): T = x
-    operator fun component2(): T = y
+	operator fun component1(): T = x
+	operator fun component2(): T = y
 
-    fun copy(): Vector2<T>
-
-
-    // geometric
-
-    val length: T
-
-    fun normalize(): Vector2<T>
-
-    infix fun dot(other: Vector2<T>): T
+	fun copy(): Vector2<T>
 
 
-    // arithmetic
+	// geometric
 
-    operator fun plus(scalar: T): Vector2<T>
-    operator fun minus(scalar: T): Vector2<T>
-    operator fun times(scalar: T): Vector2<T>
-    operator fun div(scalar: T): Vector2<T>
+	val length: T
 
-    operator fun plus(other: Vector2<T>): Vector2<T>
-    operator fun minus(other: Vector2<T>): Vector2<T>
-    operator fun times(other: Vector2<T>): Vector2<T>
-    operator fun div(other: Vector2<T>): Vector2<T>
+	fun normalize(): Vector2<T>
+
+	infix fun dot(other: Vector2<T>): T
+
+
+	// arithmetic
+
+	operator fun plus(scalar: T): Vector2<T>
+	operator fun minus(scalar: T): Vector2<T>
+	operator fun times(scalar: T): Vector2<T>
+	operator fun div(scalar: T): Vector2<T>
+
+	operator fun plus(other: Vector2<T>): Vector2<T>
+	operator fun minus(other: Vector2<T>): Vector2<T>
+	operator fun times(other: Vector2<T>): Vector2<T>
+	operator fun div(other: Vector2<T>): Vector2<T>
 }
 
 fun vector2Of(x: Float, y: Float): Vector2<Float> = FloatVector2(x, y)
@@ -50,28 +50,28 @@ fun vector2Of(x: Float, y: Float): Vector2<Float> = FloatVector2(x, y)
  */
 interface MutableVector2<T> : Vector2<T> {
 
-    override var x: T
-    override var y: T
+	override var x: T
+	override var y: T
 
-    override fun copy(): MutableVector2<T>
-
-
-    // geometric
-
-    override fun normalize(): MutableVector2<T>
+	override fun copy(): MutableVector2<T>
 
 
-    // arithmetic
+	// geometric
 
-    operator fun plusAssign(scalar: T)
-    operator fun minusAssign(scalar: T)
-    operator fun timesAssign(scalar: T)
-    operator fun divAssign(scalar: T)
+	override fun normalize(): MutableVector2<T>
 
-    operator fun plusAssign(other: Vector2<T>)
-    operator fun minusAssign(other: Vector2<T>)
-    operator fun timesAssign(other: Vector2<T>)
-    operator fun divAssign(other: Vector2<T>)
+
+	// arithmetic
+
+	operator fun plusAssign(scalar: T)
+	operator fun minusAssign(scalar: T)
+	operator fun timesAssign(scalar: T)
+	operator fun divAssign(scalar: T)
+
+	operator fun plusAssign(other: Vector2<T>)
+	operator fun minusAssign(other: Vector2<T>)
+	operator fun timesAssign(other: Vector2<T>)
+	operator fun divAssign(other: Vector2<T>)
 }
 
 fun mutableVector2Of(x: Float, y: Float): MutableVector2<Float> = FloatVector2(x, y)
