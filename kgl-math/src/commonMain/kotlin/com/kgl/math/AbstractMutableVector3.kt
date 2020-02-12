96b56b1d2a93fb324f008847049b6b8db2755a3d
Nicola Buescher <nicolalucasbuescher@gmail.com>
2020-02-12 16:15:32 -0500

    Add documentation, and FloatVector3 implementation and helper functions.

--- kgl-math/src/commonMain/kotlin/com/kgl/math/AbstractMutableVector3.kt
+++ kgl-math/src/commonMain/kotlin/com/kgl/math/AbstractMutableVector3.kt
@@ -1,18 +1,19 @@
 package com.kgl.math
 
 /**
- * Provides a skeletal implementations of the [MutableVector2] interface.
+ * Provides a skeletal implementations of the [MutableVector3] interface.
  *
  * @param T the type of component contained in the vector. The vector is invariant on its component type.
  */
-abstract class AbstractMutableVector2<T> : MutableVector2<T> {
+abstract class AbstractMutableVector3<T> : MutableVector3<T> {
 
 	override fun equals(other: Any?): Boolean {
 		if(this === other) return true
-		if (other !is AbstractMutableVector2<*>) return false
+		if (other !is AbstractMutableVector3<*>) return false
 
 		if (x != other.x) return false
 		if (y != other.y) return false
+		if (z != other.z) return false
 
 		return true
 	}
@@ -20,8 +21,9 @@ abstract class AbstractMutableVector2<T> : MutableVector2<T> {
 	override fun hashCode(): Int {
 		var result = x.hashCode()
 		result += 31 * y.hashCode()
+		result += 31 * z.hashCode()
 		return result
 	}
 
-	override fun toString(): String = "[$x, $y]"
+	override fun toString(): String = "[$x, $y, $z]"
 }
