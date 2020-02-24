a72d81516851cbe00d212f395542c2f29d0491e3
Nicola Buescher <nicolalucasbuescher@gmail.com>
2020-02-24 13:08:39 -0500

    make component function extensions

--- buildSrc/src/main/kotlin/codegen/math/GenerateMath.kt
+++ buildSrc/src/main/kotlin/codegen/math/GenerateMath.kt
@@ -21,7 +21,7 @@ open class GenerateMath : DefaultTask() {
 	private val packageName = "com.kgl.math"
 
 	private val typeList = listOf(
-		// BYTE to "0", // i
+		// BYTE to "0",
 		// SHORT to "0",
 		// INT to "0",
 		// LONG to "0L",
@@ -32,138 +32,7 @@ open class GenerateMath : DefaultTask() {
 
 	@TaskAction
 	fun generate() {
-		generateVector2()
 		generateVectors()
-	}
-
-	private fun generateVector2() {
-		buildFile(packageName, "Vector2") {
-			import("kotlin.math", "sqrt")
-
-			for ((componentType, zero) in typeList) {
-				val vectorType = ClassName(packageName, "${componentType.simpleName}Vector2")
-				val mutableVectorType = ClassName(packageName, "Mutable${componentType.simpleName}Vector2")
-				val arithmetics = listOf("plus" to "+", "minus" to "-", "times" to "*", "div" to "/")
-
-				buildClass(vectorType) {
-					modifiers(SEALED)
-
-					property("x", componentType, ABSTRACT)
-
-					property("y", componentType, ABSTRACT)
-
-					function("get") {
-						modifiers(OPERATOR)
-						parameter("index", INT)
-						controlFlow("when (index)") {
-							statement("0 -> x; 1 -> y")
-							statement("else -> throw IndexOutOfBoundsException()")
-						}
-					}
-
-					function("component1") {
-						modifiers(OPERATOR)
-						returns(componentType)
-						statement("return x")
-					}
-
-					function("component2") {
-						modifiers(OPERATOR)
-						returns(componentType)
-						statement("return y")
-					}
-
-					property("length", componentType) {
-						getter {
-							statement("return sqrt(this dot this)")
-						}
-					}
-
-					property("lengthSquared", componentType) {
-						getter {
-							statement("return this dot this")
-						}
-					}
-
-					function("normalized") {
-						returns(vectorType)
-						statement("val length = length")
-						statement("return %T(x / length, y / length)", vectorType)
-					}
-
-					function("dot") {
-						modifiers(INFIX)
-						parameter("other", vectorType)
-						returns(componentType)
-						statement("return x * other.x + y * other.y")
-					}
-
-					for ((name, op) in arithmetics) {
-						function(name) {
-							modifiers(OPERATOR)
-							parameter("other", vectorType)
-							returns(vectorType)
-							statement("return %T(x $op other.x, y $op other.y)", vectorType)
-						}
-					}
-				}
-
-				function(vectorType.simpleName) {
-					parameter("x", componentType)
-					parameter("y", componentType)
-					returns(vectorType)
-					statement("return %T(x, y)", mutableVectorType)
-				}
-
-				buildClass(mutableVectorType) {
-					superclass(vectorType)
-
-					primaryConstructor {
-						parameter("x", componentType)
-						parameter("y", componentType)
-						callSuperConstructor()
-					}
-
-					secondaryConstructor {
-						parameter("scalar", componentType) {
-							defaultValue(zero)
-						}
-						callThisConstructor("scalar", "scalar")
-					}
-
-					mutableProperty("x", componentType, OVERRIDE) {
-						initializer("x")
-					}
-
-					mutableProperty("y", componentType, OVERRIDE) {
-						initializer("y")
-					}
-
-					function("set") {
-						modifiers(OPERATOR)
-						parameter("index", INT)
-						parameter("value", componentType)
-						controlFlow("when(index)") {
-							statement("0 -> x = value; 1 -> y = value")
-							statement("else -> throw IndexOutOfBoundsException()")
-						}
-					}
-
-					function("normalize") {
-						statement("val length = length")
-						statement("x /= length; y /= length")
-					}
-
-					for ((name, op) in arithmetics) {
-						function("${name}Assign") {
-							modifiers(OPERATOR)
-							parameter("other", vectorType)
-							statement("x $op= other.x; y $op= other.y")
-						}
-					}
-				}
-			}
-		}.apply { writeTo(commonDir.get().asFile) }
 	}
 
 	private val allComponents = listOf(1 to "x", 2 to "y", 3 to "z", 4 to "w")
@@ -178,8 +47,7 @@ open class GenerateMath : DefaultTask() {
 
 				for ((componentType, zero) in typeList) {
 					val vectorType = ClassName(packageName, "${componentType.simpleName}Vector$componentCount")
-					val mutableVectorType =
-						ClassName(packageName, "Mutable${componentType.simpleName}Vector$componentCount")
+					val mutableVectorType = ClassName(packageName, "Mutable${vectorType.simpleName}")
 
 					buildClass(vectorType) {
 						modifiers(SEALED)
@@ -196,14 +64,6 @@ open class GenerateMath : DefaultTask() {
 									statement("${i - 1} -> $name")
 								}
 								statement("else -> throw IndexOutOfBoundsException()")
-							}
-						}
-
-						components.forEach { (i, name) ->
-							function("component$i") {
-								modifiers(OPERATOR)
-								returns(componentType)
-								statement("return $name")
 							}
 						}
 
@@ -345,6 +205,14 @@ open class GenerateMath : DefaultTask() {
 									statement("$name $op= other.$name")
 								}
 							}
+						}
+					}
+
+					components.forEach { (i, name) ->
+						extensionFunction(vectorType, "component$i") {
+							modifiers(OPERATOR)
+							returns(componentType)
+							statement("return $name")
 						}
 					}
 				}
