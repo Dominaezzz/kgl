5e745f444b4a4e22f79787144ab6b6a2809eb700
Nicola Buescher <nicolalucasbuescher@gmail.com>
2020-03-02 10:49:56 -0500

    refactor component handling (no need to specify component number, use index instead)

--- buildSrc/src/main/kotlin/codegen/math/GenerateMath.kt
+++ buildSrc/src/main/kotlin/codegen/math/GenerateMath.kt
@@ -3,16 +3,20 @@ package codegen.math
 import com.squareup.kotlinpoet.*
 import com.squareup.kotlinpoet.KModifier.*
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.RegularFile
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 
 @Suppress("UnstableApiUsage")
 open class GenerateMath : DefaultTask() {
+
 	@OutputDirectory
-	val outputDir = project.objects.directoryProperty()
+	val outputDir: DirectoryProperty = project.objects.directoryProperty()
 
 	@get:OutputDirectory
-	val commonDir = outputDir.file("common")
+	val commonDir: Provider<RegularFile> = outputDir.file("common")
 
 
 	private val packageName = "com.kgl.math"
@@ -36,34 +40,44 @@ open class GenerateMath : DefaultTask() {
 		vectorTypes()
 	}
 
-	private val allComponents = listOf(1 to "x", 2 to "y", 3 to "z", 4 to "w")
+	private val suppress = ClassName("kotlin", "Suppress")
+
+	private val allComponentNames = listOf("x", "y", "z", "w")
 	private val arithmetics = listOf("plus" to "+", "minus" to "-", "times" to "*", "div" to "/")
 
 	private fun vectorTypes() {
 		for (componentCount in 2..4) {
-			val components = allComponents.take(componentCount)
+			val componentNames = allComponentNames.take(componentCount)
 
 			typeList.forEach { (type, zero) ->
 				val vectorType = ClassName(packageName, "${type.simpleName}Vector$componentCount")
 				val mutableVectorType = ClassName(packageName, "Mutable${vectorType.simpleName}")
 
 				buildFile(packageName, vectorType.simpleName) {
-					import("kotlin.math", "abs")
+					indent("\t")
+
+					annotation(suppress) {
+						member("%S", "NOTHING_TO_INLINE")
+					}
+
+					when (type) {
+						INT, LONG, FLOAT, DOUBLE -> import("kotlin.math", "abs")
+					}
 					import("kotlin.math", "sqrt")
 
 					buildClass(vectorType) {
 						modifiers(SEALED)
 
-						components.forEach { (_, name) ->
-							property(name, type, ABSTRACT)
+						componentNames.forEach {
+							property(it, type, ABSTRACT)
 						}
 
 						function("get") {
 							modifiers(OPERATOR)
 							parameter("index", INT)
 							controlFlow("when (index)") {
-								components.forEach { (i, name) ->
-									statement("${i - 1} -> $name")
+								componentNames.forEachIndexed { i, it ->
+									statement("$i -> $it")
 								}
 								statement("else -> throw IndexOutOfBoundsException()")
 							}
@@ -92,11 +106,11 @@ open class GenerateMath : DefaultTask() {
 						function("normalized") {
 							returns(vectorType)
 							statement("val length = length")
-							val args = components.joinToString { (_, name) ->
+							val args = componentNames.joinToString {
 								when (type) {
 									BYTE, SHORT,
-									U_BYTE, U_SHORT -> "($name / length).to${type.simpleName}()"
-									else -> "$name / length"
+									U_BYTE, U_SHORT -> "($it / length).to${type.simpleName}()"
+									else -> "$it / length"
 								}
 							}
 							statement("return %T($args)", vectorType)
@@ -107,11 +121,11 @@ open class GenerateMath : DefaultTask() {
 								modifiers(OPERATOR)
 								parameter("other", vectorType)
 								returns(vectorType)
-								val args = components.joinToString { (_, name) ->
+								val args = componentNames.joinToString {
 									when (type) {
 										BYTE, SHORT,
-										U_BYTE, U_SHORT -> "($name $op other.$name).to${type.simpleName}()"
-										else -> "$name $op other.$name"
+										U_BYTE, U_SHORT -> "($it $op other.$it).to${type.simpleName}()"
+										else -> "$it $op other.$it"
 									}
 								}
 								statement("return %T($args)", vectorType)
@@ -123,7 +137,7 @@ open class GenerateMath : DefaultTask() {
 						superclass(vectorType)
 
 						primaryConstructor {
-							components.forEach { (_, name) ->
+							componentNames.forEach { name ->
 								parameter(name, type)
 							}
 							callSuperConstructor()
@@ -131,14 +145,14 @@ open class GenerateMath : DefaultTask() {
 
 						secondaryConstructor {
 							parameter("scalar", type)
-							callThisConstructor(*components.map { "scalar" }.toTypedArray())
+							callThisConstructor(*componentNames.map { "scalar" }.toTypedArray())
 						}
 
 						secondaryConstructor {
-							callThisConstructor(*components.map { zero }.toTypedArray())
+							callThisConstructor(*componentNames.map { zero }.toTypedArray())
 						}
 
-						components.forEach { (_, name) ->
+						componentNames.forEach { name ->
 							mutableProperty(name, type, OVERRIDE) {
 								initializer(name)
 							}
@@ -149,8 +163,8 @@ open class GenerateMath : DefaultTask() {
 							parameter("index", INT)
 							parameter("value", type)
 							controlFlow("when(index)") {
-								components.forEach { (i, name) ->
-									statement("${i - 1} -> $name = value")
+								componentNames.forEachIndexed { i, it ->
+									statement("$i -> $it = value")
 								}
 								statement("else -> throw IndexOutOfBoundsException()")
 							}
@@ -158,11 +172,11 @@ open class GenerateMath : DefaultTask() {
 
 						function("normalize") {
 							statement("val length = length")
-							components.forEach { (_, name) ->
+							componentNames.forEach {
 								val code = when (type) {
 									BYTE, SHORT,
-									U_BYTE, U_SHORT -> "$name = ($name / length).to${type.simpleName}()"
-									else -> "$name /= length"
+									U_BYTE, U_SHORT -> "$it = ($it / length).to${type.simpleName}()"
+									else -> "$it /= length"
 								}
 								statement(code)
 							}
@@ -172,11 +186,11 @@ open class GenerateMath : DefaultTask() {
 							function("${funName}Assign") {
 								modifiers(OPERATOR)
 								parameter("other", vectorType)
-								components.forEach { (_, name) ->
+								componentNames.forEach {
 									val code = when (type) {
 										BYTE, SHORT,
-										U_BYTE, U_SHORT -> "$name = ($name $op other.$name).to${type.simpleName}()"
-										else -> "$name $op= other.$name"
+										U_BYTE, U_SHORT -> "$it = ($it $op other.$it).to${type.simpleName}()"
+										else -> "$it $op= other.$it"
 									}
 									statement(code)
 								}
@@ -187,11 +201,11 @@ open class GenerateMath : DefaultTask() {
 					// extensions
 
 					function(vectorType.simpleName) {
-						components.forEach { (_, name) ->
+						componentNames.forEach { name ->
 							parameter(name, type)
 						}
 						returns(vectorType)
-						statement("return %T(${components.joinToString { it.second }})", mutableVectorType)
+						statement("return %T(${componentNames.joinToString()})", mutableVectorType)
 					}
 
 					function(vectorType.simpleName) {
@@ -200,8 +214,8 @@ open class GenerateMath : DefaultTask() {
 						statement("return %T(scalar)", mutableVectorType)
 					}
 
-					components.forEach { (i, name) ->
-						extensionFunction(vectorType, "component$i") {
+					componentNames.forEachIndexed { i, name ->
+						extensionFunction(vectorType, "component${i + 1}") {
 							modifiers(INLINE, OPERATOR)
 							returns(type)
 							statement("return $name")
@@ -212,7 +226,7 @@ open class GenerateMath : DefaultTask() {
 						modifiers(INFIX)
 						parameter("other", vectorType)
 						returns(type)
-						val result = components.joinToString(" + ") { (_, name) -> "$name * other.$name" }
+						val result = componentNames.joinToString(" + ") { "$it * other.$it" }
 							.let {
 								when (type) {
 									BYTE, SHORT,
@@ -229,17 +243,15 @@ open class GenerateMath : DefaultTask() {
 							parameter("other", vectorType)
 							returns(vectorType)
 							val args =
-								components
+								componentNames
 									.drop(1)
-									.plus(components.take(2))
+									.plus(componentNames.take(2))
 									.zipWithNext()
 									.joinToString { (c1, c2) ->
-										val name1 = c1.second
-										val name2 = c2.second
 										when (type) {
 											BYTE, SHORT,
-											U_BYTE, U_SHORT -> "($name1 * other.$name2 - other.$name1 * $name2).to${type.simpleName}()"
-											else -> "$name1 * other.$name2 - other.$name1 * $name2"
+											U_BYTE, U_SHORT -> "($c1 * other.$c2 - other.$c1 * $c2).to${type.simpleName}()"
+											else -> "$c1 * other.$c2 - other.$c1 * $c2"
 										}
 									}
 							statement("return %T($args)", vectorType)
