7e7f18638487f7a2db5f40876069098f2ee3dce9
Dominic Fischer <dominicfischer7@gmail.com>
2020-06-29 09:25:28 +0100

    Revert "Fix failure"
    This reverts commit 2b4871ce

--- kgl-core/src/jsMain/kotlin/com/kgl/core/BufferUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.kgl.core
-
-import org.khronos.webgl.Int8Array
-
-actual inline fun <T> withByteBuffer(length: Long, block: (ByteBuffer) -> T): T {
-	val array = ByteArray(length.toInt())
-	return block(ByteBuffer(array.unsafeCast<Int8Array>()))
-}
